b61d56a318f6d44858bc27ddde477331b5097816
Waffesrgud <gcor8853@740s-pl-129.local>
2016-09-15 07:35:12 -0600

    Made final edit by moving notes leftward.

--- src/monster/controller/MonsterController.java
+++ src/monster/controller/MonsterController.java
@@ -9,8 +9,8 @@ public class MonsterController
 	{
 		firstMonster= new MarshmallowMonster("Basking Suffer", 0.5, 3, true, 3, 4);
 	}
-	//* The Basking Suffer as named by the tribes that hunted it, has half an antennae, and 3 eyes.
-	//*It has a bellybutton in the shape of a heart, 3 tails instead of arms, and 4 noses.
+//* The Basking Suffer as named by the tribes that hunted it, has half an antennae, and 3 eyes.
+//*It has a belly button in the shape of a heart, 3 tails instead of arms, and 4 noses.
 	public void start()
 	{
 		System.out.println("Here is my monster: " + firstMonster);
