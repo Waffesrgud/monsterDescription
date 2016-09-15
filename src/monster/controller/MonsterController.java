1b1a6c08056113d33e5a6ee2bae3f24b047a4d2c
Waffesrgud <gcor8853@740s-pl-129.local>
2016-09-15 07:34:04 -0600

    Added some ifs while reading out the monster.

--- src/monster/controller/MonsterController.java
+++ src/monster/controller/MonsterController.java
@@ -15,10 +15,22 @@ public class MonsterController
 	{
 		System.out.println("Here is my monster: " + firstMonster);
 		System.out.println("It has this many eyes: " + firstMonster.getEyeCount());
+		if(firstMonster.getEyeCount() >= 3) {
+			System.out.println("I wonder why they need so many eyes...?");
+		}
 		System.out.println("The statement that it has a bellybutton would be " + firstMonster.getBellyButton());
 		System.out.println("It has this many antennae: " + firstMonster.getAntennaCount());
 		System.out.println("It has this many tails: " + firstMonster.getTailCount());
-		System.out.println("It has this many eyes: " + firstMonster.getNoseCount());
+		if(firstMonster.getTailCount() > 1) {
+			System.out.println("Oh, it has multiple tails? That's interesting, what do they use them for?");
+		} else if(firstMonster.getTailCount() == 1) {
+			System.out.println("Oh, a tail. That's cool.");
+		}
+			System.out.println("It has this many noses: " + firstMonster.getNoseCount());
+		if(firstMonster.getNoseCount() > 1) {
+			System.out.println("With that many noses, I bet it can smell us right now!");
+
+		}
 	}
 	
 }
